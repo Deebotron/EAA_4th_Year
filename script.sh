3c3aa43c151d8c5260e4c953d2546a6cf97df856
Deebotron <stephenbegley@gmail.com>
2014-09-26 15:44:28 +0100

    This file is changed to be an executable

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+#: Title : CPU count
+#: Date : 26/9/14	
+#: Author : Stephen
+#: Version :1.1
+#: Description :cpu count script 
+#: Options : hell no
+
 cat /proc/cpuinfo | grep processor | wc -l
 
 
